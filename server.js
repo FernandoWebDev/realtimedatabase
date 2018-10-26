3ddf9c8843c62dd9fa5913e946af7ecf54b5b2ca
fpereira <fernando.pereira@poptrade.com.br>
2018-10-26 17:32:15 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const api = require('./routes/api');
+const Pusher = require('pusher');
+
+const pusher = new Pusher({
+    appId      : '628358',
+    key        : '9f0c49c21147b125600e',
+    secret     : 'fc5d99be038ad858f098',
+    cluster    : 'us2',
+    encrypted  : true,
+  });
+
+  const channel = 'tasks';
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api', api);
+
+mongoose.connect('mongodb://localhost/tasksDb?replicaSet=rs');
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Connection Error:'));
+
+db.once('open', () => {
+    app.listen(9000, () => {
+        console.log('Node server running on port 9000');
+      });
+    
+      const taskCollection = db.collection('tasks');
+      const changeStream = taskCollection.watch();
+    
+      changeStream.on('change', (change) => {
+        console.log(change);
+        if(change.operationType === 'insert') {
+          const task = change.fullDocument;
+          pusher.trigger(
+            channel,
+            'inserted', 
+            {
+              id: task._id,
+              task: task.task,
+            }
+          ); 
+        } else if (change.operationType === 'delete') {
+          pusher.trigger(
+            channel,
+            'deleted', 
+            change.documentKey._id
+          );
+        }
+    });
+});
